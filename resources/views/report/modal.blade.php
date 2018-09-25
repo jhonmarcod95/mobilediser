5cbd94f08bfb05217ca21b13b0e586341effe3e1
jhonmarcod95 <jhonmarcod95@gmail.com>
2018-09-25 09:43:03 +0800

    Modal Logs

---
+++ resources/views/report/modal.blade.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="modal-default">
+    <div class="modal-dialog direct-chat direct-chat-warning">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="box-header">
+                    <h3 id="chat_title" class="box-title">Direct Chat</h3>
+
+                    <div class="box-tools pull-right">
+
+                        <button type="button" class="btn btn-box-tool" onclick="document.getElementById('msgFrame').contentWindow.location.reload();"><i class="fa fa-refresh"></i></button>
+                        <button type="button" class="btn btn-box-tool" data-widget="remove" data-dismiss="modal"><i class="fa fa-times"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-body">
+                <div class="box-body">
+                    <img id="merchandiserImage" src="" height="350px" width="100%">
+                </div>
+            </div>
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
